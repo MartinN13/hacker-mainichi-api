e434e9165c7e853a0ad47e73255cc5c58b348c59
Martin Nabhan <martin@impact-blue.co.jp>
2021-02-08 19:37:58 +0900

    ストーリーの保存が終わったらクライエントをビルドとデプロイして

--- src/settings.ts
+++ src/settings.ts
@@ -6,3 +6,4 @@ env.get('TZ').required();
 export const isDevelopment = env.get('NODE_ENV').asString() === 'development';
 export const DATE = env.get('DATE').asString();
 export const TableName = isDevelopment ? 'hacker-mainichi' : env.get('DYNAMO_DB_TABLE_NAME').required().asString();
+export const VERCEL_DEPLOY_HOOK_URL = env.get('VERCEL_DEPLOY_HOOK_URL').required().asString();
